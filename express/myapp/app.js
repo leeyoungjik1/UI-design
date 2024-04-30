27087edb5d61f41a20eeab6b6d106b272fb09621
leeyoungjik1 <eog91@naver.com>
2024-04-30 12:32:24 +0900

    240430

--- express/myapp/app.js
+++ express/myapp/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 // const apiHandler = require('./handlers')
-const users = require('./users.json')
+// const users = require('./users.json')
 
 // const myLogger = function(req, res, next){
 //     console.log('LOGGED')
