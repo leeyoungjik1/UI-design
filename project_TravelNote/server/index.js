f10301c7c5e81c2ed180d6e4fa3014b87d3707a6
leeyoungjik1 <eog91@naver.com>
2024-06-12 10:11:07 +0900

    vercel2

--- project_TravelNote/server/index.js
+++ project_TravelNote/server/index.js
@@ -18,7 +18,7 @@ mongoose.connect(config.MONGODB_URL)
 .then(() => console.log('데이터베이스 연결 성공'))
 .catch(e => console.log(`데이터베이스 연결 실패: ${e}`))
 
-
+app.get("/", (req, res) => res.send("Express on Vercel"));
 app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true})) // form POST 요청 데이터를 req.body에서 받을 수 있도록
